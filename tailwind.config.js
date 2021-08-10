c0bf578d5e51a36c3228f48b9521b9d0aea3ab29
Bassil-ali <githup7@gmail.com>
2021-08-10 16:53:37 +0200

    end

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Overpass", "sans-serif"],
+        hero: ["Langar", "sans-serif"],
+      },
+      colors: {
+        primary: "#303669",
+      },
+      spacing: {
+        112: "28rem",
+        128: "32rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
